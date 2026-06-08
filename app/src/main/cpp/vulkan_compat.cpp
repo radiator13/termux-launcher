f1526c3d901f99fcf73bca641c7f8b1ae6439b86
PickleHik3 <PickleHik3@users.noreply.github.com>
2026-06-08 23:10:38 +0300

    Route llama Vulkan calls through Android loader

--- app/src/main/cpp/vulkan_compat.cpp
+++
@@ -1,7 +0,0 @@
-#include <vulkan/vulkan.h>
-
-extern "C" VKAPI_ATTR void VKAPI_CALL vkGetPhysicalDeviceFeatures2(
-    VkPhysicalDevice physical_device,
-    VkPhysicalDeviceFeatures2* features) {
-    vkGetPhysicalDeviceFeatures2KHR(physical_device, features);
-}
