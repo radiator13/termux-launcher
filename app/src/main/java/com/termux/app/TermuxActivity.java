14b82073517cb98f70f61f444cf43e22505ff75e
PickleHik3 <PickleHik3@users.noreply.github.com>
2026-06-12 16:16:35 +0300

    Fix: resolve compilation errors for unexpected keyboard and motion event

--- app/src/main/java/com/termux/app/TermuxActivity.java
+++ app/src/main/java/com/termux/app/TermuxActivity.java
@@ -45,6 +45,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
